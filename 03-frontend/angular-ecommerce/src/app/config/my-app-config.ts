1638b8e396cca74bd868f839afad413effff22bd
Faye-yufan <yufanfei@usc.edu>
2022-10-22 17:50:02 -0700

    Add login/logout component

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa6yznvme3LhwbrZ5d7',
+        issuer: 'https://dev-38408907.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
