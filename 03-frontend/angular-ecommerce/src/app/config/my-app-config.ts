43d6922e1bb57d064a88aa593dc1a45cd1a19386
Faye-yufan <yufanfei@usc.edu>
2022-10-24 18:06:24 -0700

    Add VIP member access and handling browser refresh

--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -1,8 +1,8 @@
 export default {
 
     oidc: {
-        clientId: '0oa6yznvme3LhwbrZ5d7',
-        issuer: 'https://dev-38408907.okta.com/oauth2/default',
+        clientId: '0oa6zpzgexpqBXGEl5d7',
+        issuer: 'https://dev-66424822.okta.com/oauth2/default',
         redirectUri: 'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
