d3e5cd74acd7b3acaf61a101cb1a4356450f8ed7
Faye-yufan <yufanfei@usc.edu>
2022-12-17 23:01:53 -0800

    Stripe payment backend

--- 02-backend/spring-boot-ecommerce/src/main/java/com/fayeyufan/ecommerce/controller/CheckoutController.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/fayeyufan/ecommerce/controller/CheckoutController.java
@@ -1,9 +1,14 @@
 package com.fayeyufan.ecommerce.controller;
 
 
+import com.fayeyufan.ecommerce.dto.PaymentInfo;
 import com.fayeyufan.ecommerce.dto.Purchase;
 import com.fayeyufan.ecommerce.dto.PurchaseResponse;
 import com.fayeyufan.ecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,5 +27,15 @@ public class CheckoutController {
         PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
 
         return purchaseResponse;
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException{
+
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+
+        String paymentStr = paymentIntent.toJson();
+
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
     }
 }
--- 02-backend/spring-boot-ecommerce/src/main/java/com/fayeyufan/ecommerce/service/CheckoutService.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/fayeyufan/ecommerce/service/CheckoutService.java
@@ -1,9 +1,14 @@
 package com.fayeyufan.ecommerce.service;
 
+import com.fayeyufan.ecommerce.dto.PaymentInfo;
 import com.fayeyufan.ecommerce.dto.Purchase;
 import com.fayeyufan.ecommerce.dto.PurchaseResponse;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 public interface CheckoutService {
 
     PurchaseResponse placeOrder(Purchase purchase);
+
+    PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException;
 }
